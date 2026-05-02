d0e1328ffaf087d01523149dfcf7e07fb51aec55
ConzTheCodeWizzard <conzbeatz01@gmail.com>
2026-05-02 02:24:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
+const CACHE_NAME = "conzchat-v2"; // change version when updating
+
 self.addEventListener("install", e => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request));
+});
