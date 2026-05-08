b7b81bdde293d619669b86cfbcc11686616f21a6
ConzTheCodeWizzard <conzbeatz01@gmail.com>
2026-05-08 14:56:08 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyB7JVaVsJCRX1uWzxr8oR3_bDF18mnj8to",
+  authDomain: "conzchat.firebaseapp.com",
+  projectId: "conzchat",
+  storageBucket: "conzchat.firebasestorage.app",
+  messagingSenderId: "431124465784",
+  appId: "1:431124465784:web:055dbf6a8767b2f898458d"
+});
+
+const messaging = firebase.messaging();
