aa73e27e9ed7ac2fe4ae0abf3227631220b550dc
ConzTheCodeWizzard <conzbeatz01@gmail.com>
2026-05-11 23:50:48 +0100

    Update publicgroups.js

--- publicgroups.js
+++ publicgroups.js
@@ -4,3 +4,41 @@ window.openPublicGroupCreate = function(){
   show("publicGroupCreate");
 
 };
+
+window.pickGroupPhoto = function(){
+
+  document
+  .getElementById("groupPhotoInput")
+  .click();
+
+};
+
+window.selectedGroupPhoto = "";
+
+document.addEventListener("change", function(e){
+
+  if(e.target.id !== "groupPhotoInput")
+  return;
+
+  let file = e.target.files[0];
+
+  if(!file) return;
+
+  let reader = new FileReader();
+
+  reader.onload = function(){
+
+    window.selectedGroupPhoto =
+    reader.result;
+
+    document.getElementById(
+      "groupPhotoPreview"
+    ).innerHTML = `
+      <img src="${reader.result}">
+    `;
+
+  };
+
+  reader.readAsDataURL(file);
+
+});
